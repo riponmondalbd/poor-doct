029fc44ba4a452ff6fc53892dfa7f8ffbeb3b0ce
riponmondalbd <riponmondal149@gmail.com>
2025-04-13 03:44:02 +0600

    top footer make fully responsive

--- poor-doct-client/src/pages/Shared/Footer/Footer.jsx
+++ poor-doct-client/src/pages/Shared/Footer/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
     <div className="bg-[#223645] text-white">
       <div className="max-w-7xl mx-auto">
         {/* navbar logo with email send and satisfied client section */}
-        <div className="flex justify-between items-center py-10">
+        <div className="flex-row md:flex space-y-6 md:space-y-0 justify-between items-center py-10">
           {/* logo */}
-          <img className="h-20" src={logo} alt="logo" />
+          <img className="h-20 mx-auto md:mx-0" src={logo} alt="logo" />
           {/* subscribe by email */}
-          <div className="flex items-center gap-3">
+          <div className="flex justify-center md:justify-start items-center gap-3">
             <h3 className="font-bold">Subscribe:</h3>
             <div className="join">
               <input
@@ -19,19 +19,19 @@ const Footer = () => {
                 placeholder="Enter Your Email"
                 className="input input-bordered join-item text-black"
               />
-              <button className="btn btn-primary join-item">
+              <button className="btn bg-[#04c065] border-[#04c065] shadow join-item">
                 <FaTelegramPlane className="text-xl" />
               </button>
             </div>
           </div>
           {/* satisfied clients */}
-          <h3 className="flex items-center">
+          <h3 className="flex items-center justify-center md:justify-start">
             <span className="text-[#04c065] text-4xl font-medium">5M+ </span>
             Satisfied Clients
           </h3>
         </div>
         {/* border */}
-        <div class="border-b-1 border-[#7c8085] "></div>
+        <div className="border-b-1 border-[#7c8085] "></div>
         {/* navbar menu with recent and contact us items */}
       </div>
     </div>
