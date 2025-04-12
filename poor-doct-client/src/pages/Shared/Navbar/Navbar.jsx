d7b170d555ad221945334b3219490fd15ccfcf0a
riponmondalbd <riponmondal149@gmail.com>
2025-04-13 04:24:48 +0600

    add theme change option

--- poor-doct-client/src/pages/Shared/Navbar/Navbar.jsx
+++ poor-doct-client/src/pages/Shared/Navbar/Navbar.jsx
@@ -7,6 +7,13 @@ import { Link, NavLink } from "react-router";
 import logo from "../../../assets/logo.png";
 
 const Navbar = () => {
+  const [isdark, setIsdark] = useState(
+    JSON.parse(localStorage.getItem("isdark"))
+  );
+  useEffect(() => {
+    localStorage.setItem("isdark", JSON.stringify(isdark));
+  }, [isdark]);
+
   const [isSticky, setIsSticky] = useState(false);
 
   useEffect(() => {
@@ -154,6 +161,57 @@ const Navbar = () => {
           </div>
           <div className="navbar-end">
             <button className="btn bg-[#e12454] text-white">Contact Us</button>
+
+            {/* make dark mode or theme changer darka and white */}
+            <label className="toggle text-base-content">
+              <input
+                type="checkbox"
+                value="dark"
+                className="theme-controller"
+                checked={isdark === true ? false : true}
+                onChange={() => setIsdark(!isdark)}
+              />
+
+              <svg
+                aria-label="sun"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <circle cx="12" cy="12" r="4"></circle>
+                  <path d="M12 2v2"></path>
+                  <path d="M12 20v2"></path>
+                  <path d="m4.93 4.93 1.41 1.41"></path>
+                  <path d="m17.66 17.66 1.41 1.41"></path>
+                  <path d="M2 12h2"></path>
+                  <path d="M20 12h2"></path>
+                  <path d="m6.34 17.66-1.41 1.41"></path>
+                  <path d="m19.07 4.93-1.41 1.41"></path>
+                </g>
+              </svg>
+
+              <svg
+                aria-label="moon"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
+                </g>
+              </svg>
+            </label>
           </div>
         </div>
       </div>
