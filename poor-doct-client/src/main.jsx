dc720a4821570d9e2a067e3d57a773552a6da172
riponmondalbd <riponmondal149@gmail.com>
2025-04-29 18:06:39 +0600

    complete professional and care provider

--- poor-doct-client/src/main.jsx
+++ poor-doct-client/src/main.jsx
@@ -1,11 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import "./index.css";
 import router from "./Routes/router.jsx";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
--- poor-doct-client/src/pages/Home/Doctors/Doctors.jsx
+++ poor-doct-client/src/pages/Home/Doctors/Doctors.jsx
@@ -1,13 +1,84 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { Link } from "react-router";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
 const Doctors = () => {
+  const axiosPublic = useAxiosPublic();
+
+  const { data: doctors = [] } = useQuery({
+    queryKey: ["doctors"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/doctors");
+      console.log(res.data);
+      return res.data;
+    },
+  });
+
   return (
-    <div>
+    <div className="max-w-7xl mx-auto px-2 mb-24">
       <SectionTitle
         heading={"Our Doctors"}
         subHeading={"A Professional & Care Provider"}
       />
+      <div>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+          }}
+          rewind={true}
+          loop={true}
+          navigation={true}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, Navigation]}
+          className="mySwiper"
+        >
+          {doctors.map((doctor) => (
+            <SwiperSlide key={doctor._id}>
+              <Link to={"/"}>
+                {/* slider details */}
+                <div className="relative mx-3">
+                  <img
+                    className="rounded-[6px]"
+                    src={doctor.image}
+                    alt={doctor.name}
+                  />
+                  <h2 className="absolute -bottom-5 -left-3 text-center py-5 px-4 rounded-full inline-block bg-[#04c065] hover:bg-[#e12454] text-white text-[14px] font-medium">
+                    Explore <br /> More
+                  </h2>
+                </div>
+                <h2 className="text-center text-2xl font-semibold mt-6 mb-2">
+                  {doctor.name}
+                </h2>
+                <h3 className="text-center font-medium text-[#65768a]">
+                  {doctor.designation}
+                </h3>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
