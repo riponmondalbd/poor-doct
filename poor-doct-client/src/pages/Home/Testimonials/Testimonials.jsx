51196361e7bc4d55d39a29be6fc6ae5d6c20157f
riponmondalbd <riponmondal149@gmail.com>
2025-04-29 23:40:17 +0600

    complete testimonials section

--- poor-doct-client/src/pages/Home/Testimonials/Testimonials.jsx
+++ poor-doct-client/src/pages/Home/Testimonials/Testimonials.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { BiSolidQuoteAltLeft } from "react-icons/bi";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
+import { useQuery } from "@tanstack/react-query";
 import "swiper/css";
 import { Keyboard } from "swiper/modules";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
 
 const Testimonials = () => {
+  const axiosPublic = useAxiosPublic();
+  const { data: testimonials = [] } = useQuery({
+    queryKey: ["testimonials"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/testimonials");
+      console.log(res.data);
+      return res.data;
+    },
+  });
+
   return (
-    <div className="max-w-7xl mx-auto bg-red-400">
+    <div className="max-w-7xl mx-auto">
       <SectionTitle
         heading={"Testimonials"}
         subHeading={"Get Every Single Updates From Here"}
@@ -27,15 +40,25 @@ const Testimonials = () => {
           modules={[Autoplay, Keyboard]}
           className="mySwiper"
         >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
+          {testimonials.map((testimonial) => (
+            <SwiperSlide key={testimonial._id}>
+              <div className="card">
+                <figure className="px-10 pt-10">
+                  <img
+                    className="w-[200px] rounded-full mx-auto"
+                    src={testimonial.image}
+                    alt={testimonial.name}
+                  />
+                </figure>
+                <div className="card-body items-center text-center">
+                  <p className="lg:w-2/3">{testimonial.testimonial}</p>
+                  <BiSolidQuoteAltLeft className="text-5xl text-[#04c065]" />
+                  <h2 className="card-title">{testimonial.name}</h2>
+                  <p>{testimonial.designation}</p>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
