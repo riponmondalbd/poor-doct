07b593e3689655f276933f740c59ab6fb66f51da
riponmondalbd <riponmondal149@gmail.com>
2025-04-29 22:06:12 +0600

    complete Testimonials ui now i'll do backend for it

--- poor-doct-client/src/pages/Home/Home/Home.jsx
+++ poor-doct-client/src/pages/Home/Home/Home.jsx
@@ -4,6 +4,7 @@ import Banner from "../Banner/Banner";
 import Challenge from "../Challenge/Challenge";
 import Doctors from "../Doctors/Doctors";
 import ManageHealthCare from "../ManageHealthCare/ManageHealthCare";
+import Testimonials from "../Testimonials/Testimonials";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <ManageHealthCare />
       <Doctors />
       <Challenge />
+      <Testimonials />
     </div>
   );
 };
---
+++ poor-doct-client/src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Testimonials = () => {
+  return (
+    <div className="max-w-7xl mx-auto bg-red-400">
+      <SectionTitle
+        heading={"Testimonials"}
+        subHeading={"Get Every Single Updates From Here"}
+      />
+      <div>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 2,
+              spaceBetween: 50,
+            },
+          }}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
