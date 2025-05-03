b166218685ec74b2dc8a63cd0e3106da270e0151
riponmondalbd <riponmondal149@gmail.com>
2025-05-03 16:46:55 +0600

    fix bug in spacing and grid issue

--- poor-doct-client/src/pages/Doctors/DoctorProblemAward/DoctorProblemAward.jsx
+++ poor-doct-client/src/pages/Doctors/DoctorProblemAward/DoctorProblemAward.jsx
@@ -6,44 +6,42 @@ import awardWinner from "../../../assets/about/challenge/counter-icon-6.png";
 const DoctorProblemAward = () => {
   return (
     <div className="max-w-7xl mx-auto px-2">
-      <div className="md:flex gap-10 py-24">
+      <div className="md:grid md:grid-cols-3 gap-10 py-24">
         {/* Expert Doctors*/}
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center space-y-4">
           <img src={expertDoctor} alt="expert doctor" />
           <h1 className=" text-5xl font-semibold">540+</h1>
           <h3 className="flex text-[14px] text-[#04c065] font-medium items-center mb-2">
             Expert Doctors
           </h3>
           <p className="text-[#899dab] text-[15px]">
-            Our clients trust Poordoct for reliable, innovative healthcare
-            solutions, appreciating our support, quality service, and commitment
-            to excellence.
+            A famous doctor in Poordoct is known for dedicated service,
+            compassionate care, and improving health in underserved communities.
           </p>
         </div>
         {/* Problem Solve*/}
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center space-y-4">
           <img src={problemSolve} alt="problem solve" />
           <h1 className=" text-5xl font-semibold">899+</h1>
           <h3 className="flex text-[14px] text-[#04c065] font-medium items-center mb-2">
             Problem Solve
           </h3>
           <p className="text-[#899dab] text-[15px]">
-            Our clients trust Poordoct for reliable, innovative healthcare
-            solutions, appreciating our support, quality service, and commitment
-            to excellence.
+            Poordoct focuses on solving key healthcare problems like lack of
+            access, high costs, and limited awareness by offering affordable
+            services.
           </p>
         </div>
         {/* Award Winner*/}
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center space-y-4">
           <img src={awardWinner} alt="award winner" />
           <h1 className=" text-5xl font-semibold">100+</h1>
           <h3 className="flex text-[14px] text-[#04c065] font-medium items-center mb-2">
             Award Winner
           </h3>
           <p className="text-[#899dab] text-[15px]">
-            Our clients trust Poordoct for reliable, innovative healthcare
-            solutions, appreciating our support, quality service, and commitment
-            to excellence.
+            Poordoct is an award-winning healthcare initiative recognized for
+            its outstanding service, innovation, and impact in delivering care.
           </p>
         </div>
       </div>
